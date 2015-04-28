572dc7abe976316a2d8e450c3b04395fe66a2499
adrianvinuelas <adrian.vinuelas@gmail.com>
2015-04-28 16:38:03 +0200

    subidad de ficheros para realizar pruebas

---
+++ git.js
@@ -0,0 +1,85 @@
+var repoHTML = "User: <input type='text' name='user' value='adrianvinuelas'" +
+    "id='user' size='10' />" +
+    "Repo: <input type='text' name='repo' value='X-Nav-OAuth-GitHub-Fichero'" +
+    "id='repo' size='10' />" +
+    "<button type='button' id='botonform'>Grab repo data</button>" +
+    "<div id='repodata'/>";
+
+var github;
+
+function showFiles(error, contents) {
+    if (error) {
+		("#contenidorepo").html("<p>Error code: " + error.error + "</p>");
+    } else {
+		var files = [];
+		for (var i = 0, len = contents.length; i < len; i++) {
+		    files.push(contents[i].name);
+		};
+		("#contenidorepo").html("<p>Files:</p>" +
+				  "<ul id='files'><li>" +
+				  files.join("</li><li>") +
+				  "</li></ul>"
+		);
+    };
+}
+
+function obtenerRepo(){
+	var username = $("#user").val();
+	var reponame = $("#repo").val();
+	repo = github.getRepo(username, reponame);
+	repo.show(function(err, repo) {
+		if (err) {
+			$("#inforepo").html("<p>Error code: " + err.error + "</p>");
+		} else {
+			var info = "<p>Info del repositorio:</p>" +
+				   "<ul><li>Nombre: " + repo.full_name + "</li>" +
+				   "<li>Descripcion: " + repo.description + "</li>" +
+				   "<li>Creado: " + repo.created_at + "</li></ul>"
+			$("#inforepo").html(info);
+			repo.contents('master', '', showFiles);
+			$("#escritura").show();
+			$("#botonwrite").click(escribirenrepo);
+			$("#botonread").click(readrepo);
+		}
+	});
+};
+
+
+function escribirenrepo(){
+	repo.write('master', $("#fich").val(), $("#conte").val(), 'Nuevo commit', function(err) {});
+	$("#hecho").html("Escritura realizada");
+};
+
+function readrepo(){
+	console.log("voy a leer de = " + $("#fich").val());
+	repo.read('master', $("#fich").val(), function(err, data) {
+		$("#conte").html(data);
+	});
+}
+
+jQuery(document).ready(function() {
+	$("#escritura").hide();
+	$("#botontoken").click(obtenerRepo);
+
+	hello.init({
+		github : "2fd3956542947f2cd70e" //poner el identificador de mi aplicacion
+	    },{
+		redirect_uri : 'redirect.html',
+		oauth_proxy : "https://auth-server.herokuapp.com/proxy"
+	});
+
+	access = hello("github");
+  	access.login({response_type: 'code'}).then( function(){
+		auth = hello("github").getAuthResponse();
+		token = auth.access_token; //cojo el token de acceso a la aplicacion
+		console.log (token);
+		github = new Github({
+		    token: token,
+		    auth: "oauth"
+		});
+		$("#repoform").html(repoHTML);
+		$("#botonform").click(obtenerRepo);
+	}, function( e ){
+		alert('Signin error: ' + e.error.message);
+    });
+});
